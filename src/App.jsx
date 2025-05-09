695a54c2361d7c2c220a93366de817083f150c28
Yusuf <mikoofficial404@gmail.com>
2025-05-09 18:08:49 +0700

    add talwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // Import libraries dan komponen yang diperlukan
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from "./components/register";  // Import komponen Register yang sudah dibuat sebelumnya
+import Register from './components/register'; // Import komponen Register yang sudah dibuat sebelumnya
 
 // Komponen utama aplikasi
 function App() {
   return (
     <Router>
       <div>
-        <h1>Welcome to My App</h1>
+        <h1 className="text-red-500 font-extrabold text-2xl">
+          Welcome to My App
+        </h1>
 
         {/* Routing untuk halaman-halaman */}
         <Routes>
